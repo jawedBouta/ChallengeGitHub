ce8770e6cae66941d06df815a02681ae997d9ad8
JawedBo <jaywed@hotmail.fr>
2024-03-07 11:21:52 +0100

    Création du tableau JS dans script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const legumes = [];
+const nbrLegumes = 11;
+
+for(let i=0; i < nbrLegumes; i++) {
+    legumes[i] = `légumes ${i}`;
+};
+
+console.log(legumes[0]);
+console.log(legumes[nbrLegumes-1]);
